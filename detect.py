373b4f42dc830ccae8cddba73f86bc60e2af9121
xuchang.yuan <xuchang.yuan@sophgo.com>
2022-11-04 18:16:55 +0800

    modify code and sucessful run detect.py

--- detect.py
+++ detect.py
@@ -136,13 +136,13 @@ def detect(save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='./weights/yolov5x.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default=f'inference/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
+    parser.add_argument('--weights', nargs='+', type=str, default='../yolov5_weights_HeadDetection/helmet_head_person_s.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default=f'img_data', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='runs/detect', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
